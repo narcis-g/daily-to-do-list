80f287397bc6bd9c3d5c729159ec1c30ebb72d9f
narcis-g <80626049+narcis-g@users.noreply.github.com>
2023-10-27 12:54:27 +0000

    Create to do form

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
+import { useState } from 'react'
 import 'normalize.css'
+
 import Layout from './components/Layout'
+import TodoForm from './components/TodoForm'
+
+type Todo={
+  name: string;
+  complete: boolean;
+  createdAt: Date;
+};
 
 export default function App() {
+
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const createTodo = (name: string) => {
+    const newTodo = {
+      name: name,
+      complete: false,
+      createdAt: new Date ()
+    }
+    setTodos([...todos, newTodo]);
+  }
+
+  
   return (
 
-    <Layout>Daily to do list</Layout>
+    <Layout>Daily to do list
+    <TodoForm onSubmit={createTodo}></TodoForm>
+    </Layout>
   )
 }
---
+++ src/components/TodoForm.tsx
@@ -0,0 +1,27 @@
+import styled from 'styled-components'
+import { useState } from 'react'
+
+type Props = {
+  onSubmit: (name: string) => void;
+}
+
+const TodoForm = (props: Props) => {
+
+  const [name, setName] = useState('')
+
+  const submitForm = () => {
+    props.onSubmit(name);
+  }
+
+
+
+  return (
+
+    <div>
+      <input value={name} onChange={e => setName(e.target.value)} />
+      <button onClick={submitForm}>Add</button>
+    </div>
+  )
+}
+
+export default TodoForm;
